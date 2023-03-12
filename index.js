a2937ea57278e2474c71cdf2de3b838147e2d72d
brianjdowling101 <77179492+brianjdowling101@users.noreply.github.com>
2023-03-12 00:34:35 +0000

    javascriptcomments

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+// array of the possible choices
 const choices = ["rock", "paper", "scissors", "spock", "lizard"];
 
-function play(playerChoice, isComputer = false)
-let computerChoice = "";
-if (isComputer) {
-    computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    document.getElementById("computer-choice").innerHTML = `Computer chose ${computerChoice}.`;
-}
-document.getElementById("player-choice").innerHTML = `You chose ${playerChoice}.`;
-if (playerChoice === computerChoice) {
-    document.getElementById("result").innerHTML = "It's a tie!";
-}
+// Function that takes in the player's choice and whether or not it's the computer's turn
+function play(playerChoice, isComputer = false) {
+
+    // Declare a variable for the computer's choice
+    let computerChoice = "";
+
+    // If it's the computer's turn, choose a random option from the array of choices and display it
+    if (isComputer) {
+        computerChoice = choices[Math.floor(Math.random() * choices.length)];
+        document.getElementById("computer-choice").innerHTML = `Computer chose ${computerChoice}.`;
+    }
+
+    // Display the player's choice
+    document.getElementById("player-choice").innerHTML = `You chose ${playerChoice}.`;
+
+    // Determine the outcome based on the player's choice and the computer's choice
+    if (playerChoice === computerChoice) {
+        // If it's a tie, display a message saying so
+        document.getElementById("result").innerHTML = "It's a tie!";
+    }
